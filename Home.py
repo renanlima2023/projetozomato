223c17e58dc1ea10fa83729765dc6eaf60ef8625
Renan <147100834+renanlima2023@users.noreply.github.com>
2024-10-30 17:21:25 -0300

    Renomear arquivos para evitar problemas de codificação

--- Home.py
+++ Home.py
@@ -28,7 +28,7 @@ st.set_page_config(
     page_icon='🏠', 
     )   
 
-df = pd.read_csv('dataset\zomato.csv')
+df = pd.read_csv('dataset/zomato.csv')
 
 
 
--- pages/paises.py
+++ pages/paises.py
@@ -18,7 +18,7 @@ st.set_page_config(
     layout='wide'
     )
 
-df = pd.read_csv('dataset\zomato.csv')
+df = pd.read_csv('dataset/zomato.csv')
 
 
 
